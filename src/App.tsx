49c832e37579671c13281a3c76fbb09c22dc00e5
ploymahloy <patrick.mahloy@gmail.com>
2026-03-02 13:02:06 -0500

    unused Home file

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import { Routes, Route } from 'react-router-dom'
-import { Home, SearchResults } from './pages'
+import { Routes, Route } from 'react-router-dom';
+import { SearchResults } from './pages';
 
 function App() {
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
-			<Route path="/search" element={<SearchResults />} />
+			<Route path='/' element={<SearchResults />} />
 		</Routes>
 	);
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,2 +1,1 @@
-export { Home } from './Home';
 export { SearchResults } from './SearchResults';
